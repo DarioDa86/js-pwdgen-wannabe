6961995f95eb83d5eef15edb760758b0276cc877
DarioDa86 <dario.dagnelli@gmail.com>
2021-07-28 16:29:25 +0200

    corretto js

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,12 @@
 //1- richiedere nome all'utente
-var nome = prompt ("Inserisci il tuo nome")
+var nome = prompt("Inserisci il tuo nome");
 document.getElementById('nome').innerHTML += nome;
 //2- richiedere cognome all'utente
-var cognome = prompt ("Inserisci il tuo cognome")
+var cognome = prompt("Inserisci il tuo cognome");
 document.getElementById('cognome').innerHTML += cognome;
 
 //3- richiedere colore preferito all'utente
-var colore = prompt ("Inserisci il tuo colore preferito")
+var colore = prompt("Inserisci il tuo colore preferito");
 document.getElementById('colore').innerHTML += colore;
 
 const bool_class = 40
